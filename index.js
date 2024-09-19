8b4cbe65f3e3b70f75e7b391efb5ce24062f482b
vzexi <vastcreation@protonmail.com>
2024-09-19 16:23:45 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,5 +14,11 @@ app.get('/auth/discord', (request, response) => {
 	return response.sendFile('dashboard.html', { root: '.' });
 });
 
+app.get('/dashboard', (request, response) => {
+    if (!request.session.accessToken) {
+        return response.redirect('/'); // Redirect to index if not logged in
+    }
+    return response.sendFile('dashboard.html', { root: '.' });
+});
 const port = '53134';
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
